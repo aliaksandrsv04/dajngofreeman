b6da6210211d247a3de207e4b643616777f1e157
Alexander S. <aliaksandr.svirydau.04@gmail.com>
2025-10-31 23:50:22 +0100

    added some admin function

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,15 +1,30 @@
 from django.contrib import admin
-
+import csv
 from .models import Product, Category, Image, Order
 
 
 # Register your models here.
+
+@admin.action(description="To CSV")
+def make_published(modeladmin, request, queryset):
+    qs = queryset.values()
+    print(qs)
+    mylist = qs[1].keys()
+    print(mylist)
+    with open('info.csv', 'w', newline='') as csvfile:
+        fieldnames = mylist
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in qs:
+            writer.writerow(i)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')
     list_filter = ('in_stock', 'category')
     search_fields = ('name',)
+    actions = [make_published]
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
